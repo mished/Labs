7000cc5fdc5d3547282c01d198b63ee7b44a060e
Zahar Senko <senkozahar@gmail.com>
2015-11-01 13:17:15 +0300

    Reorganization

---
+++ Information Security/VDecryptor/VDecryptor/Decryptor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDecryptor
+{
+    public class Decryptor
+    {
+        private int keyLength;
+
+        public Decryptor(int keyLength)
+        {
+            this.keyLength = keyLength;
+        }
+    }
+}
