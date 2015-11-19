7dc7c965238940d5f8a143a7db75c22044eb71a6
Misha Trotsiuk <mished@users.noreply.github.com>
2015-11-20 00:14:04 +0300

    Fix space

--- Information Security/RSA/index.js
+++ Information Security/RSA/index.js
@@ -1,6 +1,6 @@
 var rsa = require('./src/rsa.js');
 module.exports = {
-	generateKeys: rsa.generateKeys,
-	encode:				rsa.encode,
-	decode:				rsa.decode
+  generateKeys: rsa.generateKeys,
+  encode:       rsa.encode,
+  decode:       rsa.decode
 };
