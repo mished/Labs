db57e802fc1232b5f939837dc984cc896e3ac110
Misha Trotsiuk <mished@users.noreply.github.com>
2016-01-27 21:10:13 +0300

    Upload some code

---
+++ Computers architecture/Compiler Optimizations/dotnet/dotnet/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace dotnet {
+    class Program {
+
+        static IEnumerable<double> Nums(long n, double x) {
+            var exp = 2;
+            while (exp <= n) {
+                yield return Math.Pow(-1.0, exp) * Math.Pow(x, exp) / exp;
+                exp += 1;
+            }
+        }
+
+        static void Main(string[] args) {
+            Console.Write("x: ");
+            var x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("n: ");
+            var n = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine();
+
+            var sw = new Stopwatch();
+
+            sw.Start();
+            var result = x + Nums(n, x).Sum();
+            sw.Stop();
+
+            Console.WriteLine($"Result: {result}");
+            Console.WriteLine($"Elapsed time: {sw.ElapsedMilliseconds}");
+        }
+    }
+}
---
+++ Information Security/ElGamal/ElGamal/ElGamal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace ElGamal {
+
+    public static class ElGamal {
+
+        public static Keys GenerateKeys(int n) {
+            var p = GetBigRandomInt(n);
+            while (!p.isProbablePrime())
+                p = GetBigRandomInt(n);
+
+            var g = GetPrimitiveRoot(p);
+            var x = GetBigRandomInt(n / 2);
+            var y = BigInteger.ModPow(g, x, p);
+
+            return new Keys {
+                PublicKey = $"{p.ToString()}:{g.ToString()}:{y.ToString()}",
+                PrivateKey = x.ToString()
+            };
+        }
+
+        private static BigInteger GetPrimitiveRoot(BigInteger modulo) {
+            var phi = modulo - 1;
+            var fact = Factorize(phi);
+            for (var i = new BigInteger(2); i < modulo; i++) {
+                var ok = true;
+                for (var j = 0; j < fact.Count && ok; j++)
+                    ok &= (BigInteger.ModPow(i, phi / fact[j], modulo) != 1);
+                if (ok)
+                    return i;
+            }
+            return -1;
+        }
+
+        private static IList<BigInteger> Factorize(BigInteger num) {
+            var res = new List<BigInteger>();
+            var n = num;
+            for (var i = 2; i * i <= n; i++)
+                if (n % i == 0) {
+                    res.Add(i);
+                    while (n % i == 0)
+                        n /= i;
+                }
+            if (n > 1)
+                res.Add(n);
+
+            return res;
+        }
+
+        private static bool isProbablePrime(this BigInteger num) {
+            var exp = num - 1;
+            for (var i = 1; i < 50; i++) {
+                if (BigInteger.ModPow(i, exp, num) != 1)
+                    return false;
+            }
+            return true;
+        }
+
+        private static BigInteger GetBigRandomInt(int n) {
+            var rng = RandomNumberGenerator.Create();
+            byte[] bytes = new byte[n / 8];
+            rng.GetBytes(bytes);
+            var res = new BigInteger(bytes);
+            while (res <= 0) {
+                rng.GetBytes(bytes);
+                res = new BigInteger(bytes);
+            }
+            return res;
+        }
+    }
+
+    public class Keys {
+        public string PublicKey { get; set; }
+        public string PrivateKey { get; set; }
+    }
+}
