bb2755c917dc5c172d020f33eb067c133c930f70
Misha Trotsiuk <mished@users.noreply.github.com>
2016-02-21 02:33:03 +0300

    Add parallel start

--- Computers architecture/SIMD/Sandbox/Program.cs
+++ Computers architecture/SIMD/Sandbox/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Drawing;
-using SIMD;
 using System.IO;
+using System.Threading.Tasks;
+using SIMD;
 
 namespace Sandbox {
     class Program {
@@ -14,9 +11,11 @@ namespace Sandbox {
 
             var path = "..\\..\\img\\";
             var sw = new Stopwatch();
+            var sw2 = new Stopwatch();
             var i = 0;
 
             Console.WriteLine("Sobel op, no Vect: ");
+            sw2.Start();
             foreach (var file in Directory.GetFiles(path, "*.bmp")) {                
                 var image = new Bitmap(file);
                 sw.Reset();
@@ -26,8 +25,14 @@ namespace Sandbox {
                 Console.WriteLine($"[{file}]: {sw.ElapsedMilliseconds}ms");
                 sobelled.Save($"{i++}.bmp");
             }
-            i = 0;
+            sw2.Stop();
+            Console.WriteLine($"Elapsed time: {sw2.ElapsedMilliseconds}");
+
+
             Console.WriteLine("\nSobel op, Vect: ");
+            i = 0;
+            sw2.Reset();
+            sw2.Start();            
             foreach (var file in Directory.GetFiles(path, "*.bmp")) {
                 var image = new Bitmap(file);
                 sw.Reset();
@@ -37,6 +42,25 @@ namespace Sandbox {
                 Console.WriteLine($"[{file}]: {sw.ElapsedMilliseconds}ms");
                 sobelled.Save($"{i++}_VECT.bmp");
             }
+            sw2.Stop();
+            Console.WriteLine($"Elapsed time: {sw2.ElapsedMilliseconds}");
+
+
+            Console.WriteLine("\nSobel op, Vect + Parallel: ");
+            i = 0;
+            sw2.Reset();
+            sw2.Start();
+            Parallel.ForEach(Directory.GetFiles(path, "*.bmp"), file => {
+                var image = new Bitmap(file);
+                var sw3 = new Stopwatch();
+                sw3.Start();
+                var sobelled = new Sobel(image, SobelFunc.Vect).Image;
+                sw3.Stop();
+                Console.WriteLine($"[{file}]: {sw3.ElapsedMilliseconds}ms");
+                sobelled.Save($"{i++}_VECTPARALLEL.bmp");
+            });
+            sw2.Stop();
+            Console.WriteLine($"Elapsed time: {sw2.ElapsedMilliseconds}");
         }
     }
 }
