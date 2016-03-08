74e3340480941e79f6fd935a06ae82c61b984a31
Misha Trotsiuk <mished@users.noreply.github.com>
2016-03-08 21:15:09 +0300

    Add threadpool lab

---
+++ Computers architecture/ThreadPool/Sandbox/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using Mished.ThreadPool;
+using SIMD;
+
+namespace Sandbox {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            var path = "..\\..\\img\\";
+            var i = 0;
+            var pool = new GoPool(20);
+            var qPool = new GoPool(1);
+
+            Directory.GetFiles(path, "*.bmp").ToList().ForEach(file => {
+                var image = new Bitmap(file);
+
+                pool.Go(() => {
+                    var sw = new Stopwatch();
+
+                    sw.Start();
+                    var sobelled = new Sobel(image, SobelFunc.Vect).Image;
+                    sw.Stop();
+
+                    qPool.Go(() => sobelled.Save($"{i++}_POOL.bmp"));
+                    Console.WriteLine($"[{file}]: {sw.ElapsedMilliseconds}ms");
+                });
+            });
+        }
+    }
+}
---
+++ Computers architecture/ThreadPool/ThreadPool/GoPool.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Mished.ThreadPool {
+
+    public class GoPool {
+        private int threadsCount;
+        private SemaphoreSlim throttler;
+        private Object thisLock = new Object();
+        private Queue<Action> tasks = new Queue<Action>();
+
+        private GoPool() { }
+
+        public GoPool(int threads) {
+            threadsCount = threads;
+            throttler = new SemaphoreSlim(threadsCount);
+        }
+
+        public void Go(Action action) {
+            tasks.Enqueue(action);
+            ProcessTask();
+        }
+
+        private async void ProcessTask() {
+            await throttler.WaitAsync();
+            new Thread(() => {
+                Action task;
+                lock (thisLock) {
+                    task = tasks.Dequeue();
+                }
+                task();
+                throttler.Release();
+            }).Start();
+        }
+
+    }
+}
