3661a16abc37b997eb8a4e9b94f95a5c375beae0
Zahar Senko <senkozahar@gmail.com>
2016-03-17 00:38:36 +0300

    fix

---
+++ Systems and Networks/UDP/UDP.Client/Client.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP {
+    public class Client {
+        UdpClient listener;
+        IPEndPoint groupEP ;
+
+        public Client(int port) {
+            listener = new UdpClient(port);
+            groupEP = new IPEndPoint(IPAddress.Any, port);
+
+            Broadcast();
+        }
+
+        private void Broadcast() {
+            while(true) {
+                var bytes = listener.Receive(ref groupEP);     
+                var mess = Encoding.ASCII.GetChars(bytes);
+                Console.WriteLine(new string(mess));
+            }
+
+        }
+
+        private string ReadMessage()
+        {
+            var message = "";
+            while (true)
+            {
+                var ch = Console.ReadKey();
+                if (ch.Key == ConsoleKey.PageDown)
+                    break;
+                message += ch.KeyChar;
+            }
+            return message;
+        }
+    }
+}
