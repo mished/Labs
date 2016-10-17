fdc188f254bf4a83aff32e978b8a4876579dcd2e
Mihail Tratsiuk <mtratsiuk@users.noreply.github.com>
2016-10-17 18:30:29 +0300

    Update lab3

--- Modeling/lab3/app.py
+++ Modeling/lab3/app.py
@@ -2,23 +2,28 @@ from random import random as rand
 import matplotlib.pyplot as plt
 import math
 
+
 def uniform_dist(a, b):
     r = rand()
-    return (a + (b - a) * r, r)
+    return a + (b - a) * r
+
 
 def normal_dist(m, sigma):
     n = 6
     r = sum([rand() for _ in range(n)])
-    return (m + sigma * (12 / n)**0.5 * r - n / 2, r)
+    return m + sigma * (12 / n)**0.5 * r - n / 2
+
 
 def exp_dist(lambd):
     r = rand()
-    return (-1 / lambd * math.log(r), r)
+    return -1 / lambd * math.log(r)
+
 
 def triangular_dist(a, b):
     l, u = a / 2, b / 2
     r1, r2 = uniform_dist(l, u), uniform_dist(l, u)
-    return (r1[0] + r2[0], r1[1] + r2[1])
+    return r1 + r2
+
 
 def draw_normal():
     m = 0
@@ -27,35 +32,67 @@ def draw_normal():
         return 1 / (2 * sigma**2 * math.pi)**0.5 * math.e**(-(x - m)**2 / (2 * sigma**2))
 
     expected = [(x, expected_func(x)) for x in frange(-5, 6, 0.1)]
-    actual = sorted(generate_seq(lambda: normal_dist(m, sigma), 100))
-    draw(expected, actual)
+    actual_func = lambda: normal_dist(m, sigma)
+    draw(expected, actual_func)
+
 
 def draw_exp():
     lambd = 0.8
-    expected = [(x, lambd * math.e**(-lambd * x)) for x in frange(0, 6, 0.1)]
-    actual = sorted(generate_seq(lambda: exp_dist(lambd), 100))
-    draw(expected, actual)
+    expected = [(x, lambd * math.e**(-lambd * x)) for x in frange(0, 12, 0.1)]
+    actual_func = lambda: exp_dist(lambd)
+    draw(expected, actual_func)
+
 
 def draw_uniform():
     a, b = 2, 10
     def expected_func(x):
         if x < a or x > b: return 0
-        return 1 / (b - a)
+        return 1.0 / (b - a)
 
     expected = [(x, expected_func(x)) for x in range(12)]
-    actual = sorted(generate_seq(lambda: uniform_dist(a, b), 100))
-    draw(expected, actual)
+    actual_func = lambda: uniform_dist(a, b)
+    draw(expected, actual_func)
 
-def draw(expected, actual):
+
+def draw_triangular():
+    a, b = 2, 10
+    c = (b - a) / 2 + a
+    def expected_func(x):
+        if x < a: return 0
+        if a <= x < c: return 2 * (x - a) / ((b - a) * (c - a))
+        if x == c: return 2 / (b - a)
+        if c < x <= b: return 2 * (b - x) / ((b - a) * (b - c))
+        return 0
+
+    expected = [(x, expected_func(x)) for x in frange(0, 12, 0.1)]
+    actual_func = lambda: triangular_dist(a, b)
+    draw(expected, actual_func)
+
+
+def draw(expected, actual_func):
     expected_x, expected_y = zip(*expected)
-    actual_x, actual_y = zip(*actual)
-    plt.plot(expected_x, expected_y, 'r', actual_x, actual_y, 'b')
-    plt.show()
+    actual_dict = {}
+    plt.ion()
+    while True:
+        for value in generate_seq(actual_func, 10000):
+            value = int(value)
+            if (actual_dict.has_key(value)):
+                actual_dict[value] += 1
+            else:
+                actual_dict[value] = 1
+        values_count = sum(actual_dict.values())
+        actual_x = sorted(actual_dict.keys())
+        actual_y = [float(actual_dict[v]) / values_count for v in actual_x]
+        plt.clf()
+        plt.plot(expected_x, expected_y, 'r', actual_x, actual_y, 'b')
+        plt.pause(0.05)
+
 
 def generate_seq(func, n):
     while n > 0:
         yield func()
         n -= 1
+
 
 def frange(l, r, s):
     c = l
@@ -63,16 +100,8 @@ def frange(l, r, s):
         yield c
         c += s
 
-def get_pirson_criteria(rand_func, n):
-    intervals_count = 10
-    values = [rand_func() for _ in range(n)]
-    min_val, max_val = math.floor(min(values)), math.ceil(max(values))
-    step = float(max_val - min_val) / intervals_count
-    cur = min_val + step
-    pirson = 0
-    while cur <= max_val:
-        count = sum([1 for v in values if cur - step < v <= cur])
-        pirson += (count - n * step)**2 / (n * step)
-        cur += step
 
-    return pirson
+#draw_normal()
+#draw_exp()
+#draw_uniform()
+draw_triangular()
